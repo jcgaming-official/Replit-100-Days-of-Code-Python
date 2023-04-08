386a7d0dbcbaf1c5b8116a91cd82eed6d63e10be
jcgaming-official <77482435+jcgaming-official@users.noreply.github.com>
2023-04-08 13:32:32 +0800

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,24 @@
+# print("Hello Replit")
+
+# print("""Hello 
+#             World
+#               and
+#                 Replit""")
+# print("= MUSIC+ =")
+# print("> Songs")
+# print("> Albums")
+# print("> Artists")
+#Day 1 Challenges
+#1
+print("John Carl")
+print("April 8, 2023")
+
+#2
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
+
+#3
+print("I am feeling 🙂")
+#4
+print("You can follow my progress at replit.com/@JcGaming")
