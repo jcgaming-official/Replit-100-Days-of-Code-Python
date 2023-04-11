1d587688df6f28090bde63c0894b3b2b823a1277
jcgaming-official <77482435+jcgaming-official@users.noreply.github.com>
2023-04-11 22:53:30 +0800

    Create day4.py

---
+++ day4.py
@@ -0,0 +1,11 @@
+# print("Hello there?")
+# print("Let me ask you a questions")
+# print()
+# like = input("What things do you like? ")
+# hate = input("What things do you hate? ")
+# family = input("what is the name of your family? ")
+# friend = input("what is your name of your friend? ")
+
+# print("You like the ", like, " too?, well I hate the ", hate, "too.", family, "name was awesome! and you have a friend called ", friend, " is so nice too")
+
+print("\033[36m" , "Hello Colors")
