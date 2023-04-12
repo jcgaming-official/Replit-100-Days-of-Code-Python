b9f44c0b8e9de5e1bec1194918c4f33f387a4b2b
jcgaming-official <77482435+jcgaming-official@users.noreply.github.com>
2023-04-12 23:12:29 +0800

    Create day5.py

---
+++ day5.py
@@ -0,0 +1,51 @@
+# myName = input("What's your name?: ")
+# if myName == "David":
+#   print("Welcome Dude!")
+
+# myName = input("What's your name?: ")
+# if myName == "David":
+#  print("Welcome Dude!")
+#  print("You're just the baldest dude I've ever seen")
+# else:
+#  print("Who on earth are you?!")
+
+# catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+# if catsOrDogs == "cats":
+#   print("Meow")
+# else:
+#   print("Woof")
+
+# catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+# if catsOrDogs == "cat":
+#   print("Meow")
+# else:
+#   print("Woof")
+
+# catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+# if catsOrDogs == "cat":
+#   print("Meow")
+# else:
+#   print("Woof")
+
+# drink = input("Do you prefer coffee or tea?")
+# if drink == "coffee":
+#   print("Tea is better.")
+# else:
+#   print("Excellent choice.")
+
+print("League of Legends Quiz Time!")
+dog = input("Who is Dog? Nasus or Warwick?")
+if dog == "Nasus":
+  print("Correct!")
+else:
+  print("It's Nasus :D")
+  item = input("What is better item? Triforce or Black Clever?")
+  if item == "Triforce":
+    print("I agree, Triforce is better")
+  else:
+    print("Triforce os better! ")
+    role = input("What is the hardest role? ADC or Jungle?")
+    if role == "ADC":
+      print("Nope")
+    else:
+      print("Correct")
