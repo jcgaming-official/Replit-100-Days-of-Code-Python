dcf76ecb8e0ed7c3fac8e5f76be6ac2fcf67bcf6
jcgaming-official <77482435+jcgaming-official@users.noreply.github.com>
2023-04-14 00:06:57 +0800

    Update day6.py

--- day6.py
+++ day6.py
@@ -37,6 +37,57 @@ elif season == "summer":
 elif season == "autumn":
   print("The leaves are changing and the air is crisp. Enjoy!")
 elif season == "winter":
-      print("Stay warm by the fire and watch the snow fall.")
+      print("Stay warm by the fire and watch the snow fall.")#print("SECURE LOGIN")
+#username = input("Username > ")
+#if username == "mark":
+#  print("Welcome Mark!")
+#elif username == "suzanne":
+#  print("Hey there Suzanne!")
+#else:
+#  print("Go away!")
+
+#print("SECURE LOGIN")
+#username = input("Username > ")
+#password = input("Password> ")
+
+#if username == "mark" and password == "password":
+# print("Welcome Mark!")
+#elif username == "suzanne" and password == "Su74nne":
+# print("Hey there Suzanne!")
+#else:
+# print("Go away!")
+
+#print("SECURE LOGIN")
+#username = input("Username > ")
+
+#if username == "mark":
+#  print("Welcome Mark!")
+#elif username == "suzanne":
+#  print("Hey there Suzanne!")
+#else:
+#  print("Go away!")
+
+#season = input("what is your favorite season?")
+#if season == "spring":
+#  print("Ah! The birds are chirping and flowers blooming.")
+#elif season == "summer":
+#  print("Catch some sun and cool off with a lemonade.")
+#elif season == "autumn":
+#  print("The leaves are changing and the air is crisp. Enjoy!")
+#elif season == "winter":
+#      print("Stay warm by the fire and watch the snow fall.")
+#else: 
+# print("I don't know that season. Please try again.")
+
+print("Enter your Credentials")
+Username = input("Username: ")
+Password = input("Password: ")
+
+if Username == "john carl" and Password == "quieta":
+ print("Welcome John Carl!")
+elif Username == "Quieta" and Password == "John Carl":
+ print("Welcome John Carl!")
+else:
+ print("Wrong Credentials!")
 else: 
  print("I don't know that season. Please try again.")
