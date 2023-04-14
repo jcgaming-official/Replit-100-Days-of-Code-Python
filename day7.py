ffa3cb2fb712c4d8f00e9b7644c95095edb8b4ec
jcgaming-official <77482435+jcgaming-official@users.noreply.github.com>
2023-04-14 23:04:17 +0800

    Create day7.py

---
+++ day7.py
@@ -0,0 +1,43 @@
+#pylint:disable=E0001
+#pylint:disable=E0001
+#tvShow = input("What is your favorite tv show? ")
+#if tvShow == "Tv":
+#  print("Ugh, why?")
+#  faveCharacter = input("Who is your favorite show? ")
+#  if faveCharacter == "Nba":
+#    print("Right answer")
+#  else:
+#    print("Ok")
+#elif tvShow == "Netflix":
+#  print("Nice")
+#else:
+#  print("Yeah, that's cool and all…")
+
+#tvShow = input("What is your favourite tv show? ")
+#if tvShow == "peppa pig":
+#  print("Ugh, why?")
+#  faveCharacter = input("Who is your favourite character? ")
+#  if faveCharacter == "daddy pig":
+#    print("Right answer")
+#  else:
+#    print("Nah, Daddy Pig's the greatest")
+#elif tvShow == "paw patrol":
+#  print("Aww, sad times")
+#else:
+#  print("Yeah, that's cool and all…")
+
+#order = input("What would you like to order: pizza or hamburger? ")
+#if order == "hamburger":
+#	print("Thank you.")
+#cheese = input("Do you want cheese?")
+#if cheese == "yes":
+#  print("You got it.")
+#else: 
+#   print("No cheese it is.")
+#if order == "pizza":
+#	print("Pizza coming up.")
+#toppings = input("Do you want pepperoni on that?")
+#if toppings == "yes":
+#    print("We will add pepperoni.")
+#else:
+#    print("Your pizza will not have pepperoni.")
